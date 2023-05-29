c3c02971d743e9206f551e9fced623770cc0d076
ra1ya <132841264+ra1ya@users.noreply.github.com>
2023-05-29 23:03:29 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using homeworkreverse.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace homeworkreverse.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+       
+        [HttpPost]
+        public IActionResult Index(string name)
+        {
+            
+            char[] charArry = name.ToCharArray();
+            Array.Reverse(charArry);
+            string reversedName = new string(charArry);
+            ViewBag.ReversedName = reversedName;
+            return View();
+            
+
+
+        }
+
+        
+    }
+}
